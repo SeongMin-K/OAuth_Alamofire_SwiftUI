0c947e60e40ac70ae84001f5774fd7527b2d98e3
SeongMin-K <eo1tks3@naver.com>
2022-01-05 13:49:08 +0900

    Initial Commit

---
+++ OAuth_Alamofire_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OAuth_Alamofire_SwiftUI
+//
+//  Created by SeongMinK on 2022/01/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OAuth_Alamofire_SwiftUI/OAuth_Alamofire_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OAuth_Alamofire_SwiftUIApp.swift
+//  OAuth_Alamofire_SwiftUI
+//
+//  Created by SeongMinK on 2022/01/05.
+//
+
+import SwiftUI
+
+@main
+struct OAuth_Alamofire_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
